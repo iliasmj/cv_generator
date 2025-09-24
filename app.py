fc1088eed0731cd06647dd0299cb5f8cce48dfd9
iliasmj <iliasmj@gmail.com>
2025-09-24 21:15:57 +0200

    construct and render html view_cv template

--- app.py
+++ app.py
@@ -46,17 +46,66 @@ class Cv:
         self.experiences = []
         self.educations = []
 
-    def add_personal_data(self, key, value):
-        self.personal_data[key] = value
-    
-    def add_about_me(self, key, value):
-        self.about_me[key] = value
+    #gets personal data from index' form
+    def get_personal_data(self):
+        self.personal_data["name"] = flask.request.args.get("name")
+        self.personal_data["first_name"] = flask.request.args.get("first_name")
+        self.personal_data["birth_date"] = flask.request.args.get("birth_date")
+        self.personal_data["nationality"] = flask.request.args.get("nationality")
+        self.personal_data["phone_number"] = flask.request.args.get("phone_number")
+        self.personal_data["email"] = flask.request.args.get("email")
+        self.personal_data["address"] = flask.request.args.get("address")
+        self.personal_data["driver_license"] = flask.request.args.get("driver_license")
 
-    def add_experience(self, experience):
-        self.experiences.append(experience)
-    
-    def add_education(self, education):
-        self.educations.append(education)
+    #gets about me data from index' form
+    def get_about_me(self):
+        self.about_me["job_title"] =flask.request.args.get("about_job_title")
+        self.about_me["years_experience"] = flask.request.args.get("years_experience")
+        self.about_me["bio"] = flask.request.args.get("bio")
+        self.about_me["skills"] = flask.request.args.getlist("skill")
+
+        #constructs Language object by geting languages and proficiency levels lists
+        # + gather them in a list as Language object and pass into myself add_about_me function
+        languagesList = flask.request.args.getlist("language")
+        proficiencyLevelsList = flask.request.args.getlist("proficiency")
+        languagesObjectsList = []
+        for i in range (len(languagesList)):
+            language = languagesList[i]
+            level = proficiencyLevelsList[i]
+            newlanguage = Language(language, level)
+#            print("New Language : ") #---------------------------------------CHECK
+#            print(vars(newlanguage))  #---------------------------------------CHECK
+            languagesObjectsList.append(newlanguage)
+        self.about_me["languages"] = languagesObjectsList
+
+    #gets experiences data from index's form
+    def get_experiences(self):
+        exp_job_title = flask.request.args.getlist("exp_job_title")
+        employer = flask.request.args.getlist("employer")
+        exp_location = flask.request.args.getlist("exp_location")
+        exp_from = flask.request.args.getlist("exp_from")
+        exp_to = flask.request.args.getlist("exp_to")
+        #constructs Experience objects by gathering in order each experiences data
+        for i in range (len(exp_job_title)):
+            activities = flask.request.args.getlist("activity_" + str(i+1))
+            newExperience = Experience(exp_job_title[i], employer[i], exp_location[i], exp_from[i], exp_to[i], activities)
+#            print("New Experience : ") #---------------------------------------CHECK
+#            print(vars(newExperience)) #---------------------------------------CHECK
+            self.experiences.append(newExperience)
+
+    #gets educations data from index's form
+    def get_educations(self) :
+        institution = flask.request.args.getlist("institution")
+        edu_location = flask.request.args.getlist("edu_location")
+        edu_from = flask.request.args.getlist("edu_from")
+        edu_to = flask.request.args.getlist("edu_to")
+        program_title = flask.request.args.getlist("program_title")
+        #constructs Education objects by gathering in order each educations data
+        for i in range (len(institution)):
+            newEducation = Education(institution[i], edu_location[i], edu_from[i], edu_to[i], program_title[i])
+#            print("New Education : ") #---------------------------------------CHECK
+#            print(vars(newEducation)) #---------------------------------------CHECK
+            self.educations.append(newEducation)
 
 @app.route("/")
 def index():
@@ -65,64 +114,12 @@ def index():
 @app.route("/view_cv")
 def view_cv():
     myCv = Cv();
+    myCv.get_personal_data()
+    myCv.get_about_me()
+    myCv.get_experiences()
+    myCv.get_educations()
 
-    #gets personal data from index' form
-    myCv.add_personal_data("name", flask.request.args.get("name"))
-    myCv.add_personal_data("first_name", flask.request.args.get("first_name"))
-    myCv.add_personal_data("birth_date", flask.request.args.get("birth_date"))
-    myCv.add_personal_data("nationality", flask.request.args.get("nationality"))
-    myCv.add_personal_data("phone_number", flask.request.args.get("phone_number"))
-    myCv.add_personal_data("email", flask.request.args.get("email"))
-    myCv.add_personal_data("address", flask.request.args.get("address"))
-    myCv.add_personal_data("driver_license", flask.request.args.get("driver_license"))
+#    print("myCv : ") #---------------------------------------CHECK
+#    print(vars(myCv)) #---------------------------------------CHECK
 
-    #gets about_me data from index' form
-    myCv.add_about_me("job_title", flask.request.args.get("about_job_title"))
-    myCv.add_about_me("years_experience", flask.request.args.get("years_experience"))
-    myCv.add_about_me("bio", flask.request.args.get("bio"))
-    myCv.add_about_me("skills", flask.request.args.getlist("skill"))
-
-    #constructs Language object by geting languages and proficiency levels lists + gather them in a list as Language object and pass into myCV add_about_me function
-    languagesList = flask.request.args.getlist("language")
-    proficiencyLevelsList = flask.request.args.getlist("proficiency")
-    languagesObjectsList = []
-    for i in range (len(languagesList)):
-        language = languagesList[i]
-        level = proficiencyLevelsList[i]
-        newlanguage = Language(language, level)
-        print("New Language : ") #---------------------------------------CHECK
-        print(vars(newlanguage))  #---------------------------------------CHECK
-        languagesObjectsList.append(newlanguage)
-    myCv.add_about_me("languages", languagesObjectsList)
-
-    #gets experiences data from index's form
-    exp_job_title = flask.request.args.getlist("exp_job_title")
-    employer = flask.request.args.getlist("employer")
-    exp_location = flask.request.args.getlist("exp_location")
-    exp_from = flask.request.args.getlist("exp_from")
-    exp_to = flask.request.args.getlist("exp_to")
-    #constructs Experience objects by gathering in order each experiences data
-    for i in range (len(exp_job_title)):
-        activities = flask.request.args.getlist("activity_" + str(i+1))
-        newExperience = Experience(exp_job_title[i], employer[i], exp_location[i], exp_from[i], exp_to[i], activities)
-        print("New Experience : ") #---------------------------------------CHECK
-        print(vars(newExperience)) #---------------------------------------CHECK
-        myCv.add_experience(newExperience)
-
-    #gets educations data from index's form
-    institution = flask.request.args.getlist("institution")
-    edu_location = flask.request.args.getlist("edu_location")
-    edu_from = flask.request.args.getlist("edu_from")
-    edu_to = flask.request.args.getlist("edu_to")
-    program_title = flask.request.args.getlist("program_title")
-    #constructs Education objects by gathering in order each educations data
-    for i in range (len(institution)):
-        newEducation = Education(institution[i], edu_location[i], edu_from[i], edu_to[i], program_title[i])
-        print("New Education : ") #---------------------------------------CHECK
-        print(vars(newEducation)) #---------------------------------------CHECK
-        myCv.add_education(newEducation)
-    
-    print("my Cv : ") #---------------------------------------CHECK
-    print(vars(myCv)) #---------------------------------------CHECK
-
-    return flask.render_template("view_cv.html")
+    return flask.render_template("view_cv.html", cv=myCv)
